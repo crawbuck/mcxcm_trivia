fa25f4baf997cfcdc0acffc0ec9554441ff347eb
Michael Crawbuck <michael@brooklinen>
2023-01-22 14:15:38 -0500

    Adds firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  runtimeConfig: {
+    // Private config that is only available on the server
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+     // Config within public will be also exposed to the client
+     public: {
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY
+    }
+  }
 })
